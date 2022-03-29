4c07fc69ddf9d678be083b591c77a5106ad3750a
Imran Hossen <freelancerimran81@gmail.com>
2022-03-29 12:42:48 +0600

    app.js updated

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/friends' element={<Friends/>}></Route>
+        <Route path='/friends' element={<Friends />}></Route>
         <Route path='/friend/:friendId' element={<FriendDetail></FriendDetail>} ></Route>
         <Route path='/posts' element={<Posts></Posts>}>
-          <Route path=':postId' element={<PostDetail/>}></Route> 
+          <Route path=':postId' element={<PostDetail />}></Route>
         </Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='*' element={<NotFound/>}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   );
