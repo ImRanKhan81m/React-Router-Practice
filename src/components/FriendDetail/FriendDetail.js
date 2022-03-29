b9c263b94eb2aab7dbe0e04f7a90810d1c378e12
Imran Hossen <freelancerimran81@gmail.com>
2022-03-29 12:43:19 +0600

    friend details updated

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
-    const {friendId} = useParams();
-    const [friend, setFriend] =useState({});
-    useEffect(()=>{
+    const { friendId } = useParams();
+    const [friend, setFriend] = useState({});
+    useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
         fetch(url)
-        .then(res => res.json())
-        .then(data => setFriend(data))
-    },[friendId])
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    }, [friendId])
     return (
         <div>
             <h2>This is detail of a bondhu : {friendId}</h2>
