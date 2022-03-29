120af503c1129e3f5fe4f8319619c250bb132f27
Imran Hossen <freelancerimran81@gmail.com>
2022-03-29 12:42:00 +0600

    updated

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const PostDetail = () => {
-    const {postId} = useParams();
-    const [post , setPost] = useState({});
-    useEffect( ()=>{
+    const { postId } = useParams();
+    const [post, setPost] = useState({});
+    useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/posts/${postId}`
         fetch(url)
-        .then(res => res.json())
-        .then(data => setPost(data))
-    },[postId]);
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [postId]);
     return (
         <div>
             <h4>This is Post Detail for: {postId}</h4>
