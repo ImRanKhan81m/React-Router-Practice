fc7d173255f8da149463496d4bee83292e0939c9
Imran Hossen <freelancerimran81@gmail.com>
2022-03-29 12:43:49 +0600

    post js updated

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,21 +4,21 @@ import { Link, Outlet } from 'react-router-dom';
 const Posts = () => {
     const [posts, setPosts] = useState([]);
 
-    useEffect( ()=>{
+    useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/posts`
         fetch(url)
-        .then(res => res.json())
-        .then(data => setPosts(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setPosts(data));
+    }, [])
     return (
         <div>
             <h2>Every posts Facebook ever had: {posts.length}</h2>
             {
-                posts.map(post => 
-                <Link 
-                key={post.id} 
-                to={`/posts/${post.id}`}>{post.id} 
-                </Link>) 
+                posts.map(post =>
+                    <Link
+                        key={post.id}
+                        to={`/posts/${post.id}`}>{post.id}
+                    </Link>)
             }
             <Outlet></Outlet>
         </div>
