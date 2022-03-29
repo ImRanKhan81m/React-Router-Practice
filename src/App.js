951e1fa0334d8c4f39517387613242fd633fe6a1
Imran Hossen <freelancerimran81@gmail.com>
2022-03-29 17:07:32 +0600

    rest countries details added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Countries from './components/Countries/Countries';
+import CountryDetail from './components/CountryDetail/CountryDetail';
 import FriendDetail from './components/FriendDetail/FriendDetail';
 import Friends from './components/Friends/Friends';
 import Header from './components/Header/Header';
@@ -23,6 +24,7 @@ function App() {
           <Route path=':postId' element={<PostDetail />}></Route>
         </Route>
         <Route path='/countries' element={<Countries/>}></Route>
+        <Route path='/country/:countryName' element={<CountryDetail></CountryDetail>}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
